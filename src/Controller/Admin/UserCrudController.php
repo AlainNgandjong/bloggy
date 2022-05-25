6d21dbb991ab543ab11650c1e8317d42ab353d9b
AlainNgandjong <a.ngandjong@gmail.com>
2022-05-25 10:37:23 +0200

    add id in post index only and password only on forms + passwordType + setRequired

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -10,6 +11,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\HiddenField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -21,9 +23,13 @@ class UserCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        yield Field::new('id')->onlyOnIndex();
         yield TextField::new('name');
         yield EmailField::new('email');
-        yield Field::new('password');
+        yield Field::new('password')
+            ->setFormType(PasswordType::class)
+            ->onlyOnForms()
+            ->setRequired($pageName === Crud::PAGE_NEW);
 //        yield BooleanField::new('enabled')
 //            ->renderAsSwitch(false);
         $roles = ['ROLE_SUPER_ADMIN', 'ROLE_ADMIN', 'ROLE_MODERATOR', 'ROLE_USER'];
