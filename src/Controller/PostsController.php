4859ae838dad5a0233e8cc67ebccdb4f00554d2c
AlainNgandjong <a.ngandjong@gmail.com>
2022-06-02 01:03:28 +0200

    Make use of Requirement added symfony 6.1

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -9,12 +9,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+
 
 class PostsController extends AbstractController
 {
     public function __construct(Private PostRepository $postRepository){}
 
-    #[Route('%app.admin.path%', name: 'app_home', methods: ['GET'])]
+    #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(PostRepository $postRepository, PaginatorInterface $paginator, Request $request): Response
     {
         // TODO filters to select only published posts
@@ -33,21 +35,21 @@ class PostsController extends AbstractController
         return $this->render('posts/index.html.twig', compact('pagination'));
     }
 
+
+
     #[Route(
-        '/posts/{year}/{month}/{day}/{slug}',
+        '/posts/{date}/{slug}',
         name: 'app_posts_show',
         requirements: [
-            'year' => '[0-9]{4}',
-            'month' => '[0-9]{2}',
-            'day' => '[0-9]{2}',
-            'slug' => '[a-z0-9\-]+',
+            'date' => Requirement::DATE_YMD,
+            'slug' => Requirement::ASCII_SLUG,
         ],
         methods: ['GET']
     )]
-    public function show(int $year, int $month, int $day, string $slug): Response
+    public function show(string $date, string $slug): Response
     {
 
-        $post = $this->postRepository->findOneByPublishDateAndSlug($year, $month, $day, $slug);
+        $post = $this->postRepository->findOneByPublishDateAndSlug($date, $slug);
 
         if(!$post){
             throw $this->createNotFoundException("Post not found.");
