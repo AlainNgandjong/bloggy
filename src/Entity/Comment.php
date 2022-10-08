be99be1772f9a9a56904a233659fbf73f6fa4b90
AlainNgandjong <a.ngandjong@gmail.com>
2022-10-08 06:00:36 +0200

    Create COMMENT table + migration

---
+++ src/Entity/Comment.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\TimestampableTrait;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+#[ORM\Table(name: '`comments`')]
+class Comment
+{
+    use TimestampableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(type: 'text')]
+    private ?string $content = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $isActive = false;
+
+    #[ORM\ManyToOne(targetEntity: Post::class, inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Post $post = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function isIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getPost(): ?Post
+    {
+        return $this->post;
+    }
+
+    public function setPost(?Post $post): self
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+
+}
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -6,6 +6,8 @@ namespace App\Entity;
 use App\Repository\PostRepository;
 use App\Entity\Traits\SluggerTrait;
 use App\Entity\Traits\TimestampableTrait;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation\Slug;
 
@@ -39,6 +41,16 @@ class Post
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'posts')]
     #[ORM\JoinColumn(nullable: false)]
     private $author;
+
+    #[ORM\OneToMany(mappedBy: 'post', targetEntity: Comment::class, orphanRemoval: true)]
+    private $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
+
+
 
     public function getId(): ?int
     {
@@ -105,4 +117,36 @@ class Post
 //            'slug' =>  $this->getSlug()
 //        ];
 //    }
+
+/**
+ * @return Collection<int, Comment>
+ */
+public function getComments(): Collection
+{
+    return $this->comments;
+}
+
+public function addComment(Comment $comment): self
+{
+    if (!$this->comments->contains($comment)) {
+        $this->comments[] = $comment;
+        $comment->setPost($this);
+    }
+
+    return $this;
+}
+
+public function removeComment(Comment $comment): self
+{
+    if ($this->comments->removeElement($comment)) {
+        // set the owning side to null (unless already changed)
+        if ($comment->getPost() === $this) {
+            $comment->setPost(null);
+        }
+    }
+
+    return $this;
+}
+
+
 }
